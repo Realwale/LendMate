1c78ef44b85ccf68a1b5b531d2f45c4fb29583fc
Realwale <olawaleagboola41@gmail.com>
2024-05-20 12:02:49 +0100

    initialise LendMate spring boot microservice project

---
+++ src/main/java/com/backend/lendmate/LendMateApplication.java
@@ -0,0 +1,13 @@
+package com.backend.lendmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LendMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/lendmate/LendMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.lendmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
